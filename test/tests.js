0d562a7153d85cb1da9bd9cb68fa7d4be129035f
Easton Gorishek <ieaston26@gmail.com>
2019-02-06 19:41:40 -0800

    tests

--- test/tests.js
+++ test/tests.js
@@ -1,5 +1,30 @@
 const test = QUnit.test;
 
-test('', function(assert) {
+function even(number) {
+ if(number % 2 === 0) {
+   return true;
+ }
+ else {
+   return false;
+ }
+}
 
+
+test('returns true if even', function(assert) {
+  const number = 10;
+  const expect = true;
+  const result = even(number);
+
+  assert.equal(result, expect);
 });
+
+test('returns false if number is odd', function(assert) {
+  const number = 5;
+  const expect = false;
+  const result = even(number);
+
+  assert.equal(result, expect);
+});
+
+
+
